03c6db51a5d0ca353704a04f048412b9191b16c3
허수진/회원서비스팀 <sujin.heo@musinsa.com>
2024-04-21 17:16:59 +0900

    feat: add dns

--- src/components/section-header/index.js
+++ src/components/section-header/index.js
@@ -5,6 +5,7 @@ function SectionHeader({ title }) {
   return (
     <div className="section-header-wrapper">
       <div className="section-header">
+      <meta name="google-site-verification" content="Zlcd6hBfFBB4bPhuqK0Ou8LDY5igOHZw1zEuAACcTfA" />
         <h2>{title}</h2>
       </div>
     </div>
