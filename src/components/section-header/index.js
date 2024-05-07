a174e1c52024254007d36b283406f2c2a961bfd3
허수진/회원서비스팀 <sujin.heo@musinsa.com>
2024-05-07 17:19:50 +0900

    feat: dns

--- src/components/section-header/index.js
+++ src/components/section-header/index.js
@@ -5,7 +5,12 @@ function SectionHeader({ title }) {
   return (
     <div className="section-header-wrapper">
       <div className="section-header">
-      <meta name="google-site-verification" content="Zlcd6hBfFBB4bPhuqK0Ou8LDY5igOHZw1zEuAACcTfA" />
+        <meta name="google-site-verification"
+              content="Zlcd6hBfFBB4bPhuqK0Ou8LDY5igOHZw1zEuAACcTfA"/>
+
+        <meta name="naver-site-verification"
+              content="120fd2d2cc96a4ae55bea23b0e089aeea8f147a9"/>
+
         <h2>{title}</h2>
       </div>
     </div>
